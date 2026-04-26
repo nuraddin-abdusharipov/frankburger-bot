0645c61f21abd90a73b89373953e229859c43eaf
nuraddin-abdusharipov <breakingnew770@gmail.com>
2026-04-26 17:15:29 +0500

    init

--- index.js
+++ index.js
@@ -6,24 +6,20 @@ const app = express()
 const TOKEN = "8771407234:AAFlVx8y1S7lrI8Zz0yThThR2JW0iMkDtwc"
 const URL = "https://frankbuger.onrender.com"
 
-// Webhook mode
 const bot = new TelegramBot(TOKEN)
-bot.setWebHook(`${URL}/bot${TOKEN}`)
 
 app.use(express.json())
 
-// Telegram webhook
+// webhook route
 app.post(`/bot${TOKEN}`, (req, res) => {
-    console.log("Webhook keldi!") // DEBUG
+    console.log("Webhook keldi!")
     bot.processUpdate(req.body)
     res.sendStatus(200)
 })
 
-// /start komandasi
+// start
 bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id
-
-    bot.sendMessage(chatId, "Ilovani oching 👇", {
+    bot.sendMessage(msg.chat.id, "Ishlayapti ✅", {
         reply_markup: {
             inline_keyboard: [
                 [
@@ -39,10 +35,20 @@ bot.onText(/\/start/, (msg) => {
     })
 })
 
-// test route
+// test
 app.get('/', (req, res) => {
     res.send("Bot ishlayapti ✅")
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log("Server running..."))
+
+app.listen(PORT, async () => {
+    console.log("Server running...")
+
+    try {
+        await bot.setWebHook(`${URL}/bot${TOKEN}`)
+        console.log("Webhook o‘rnatildi ✅")
+    } catch (err) {
+        console.error(err)
+    }
+})
