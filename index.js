e8ccbce63ceb75f8b8e135dd1b20abb3ab509f14
Nuraddin Abdusharipov <breakingnew770@gmail.com>
2026-06-24 15:35:29 +0500

    Use environment variable for BOT_TOKENv
    Replaced hardcoded BOT_TOKEN with environment variable.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ⚠️ TOKENNI keyin .env ga o‘tkazganing yaxshi
-const BOT_TOKEN = "8771407234:AAERI4Thh8HHTokRi8QoK4b3cPltf72nmU4";
+const BOT_TOKEN = process.env.TOKEN;
 
 if (!BOT_TOKEN) {
   throw new Error("BOT_TOKEN topilmadi.");
